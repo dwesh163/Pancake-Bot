e14fd105960169bd8f710319c5aec4c77f8b5204
dwesh163 <ehouarn.duriaux@protonmail.com>
2023-09-01 10:43:13 +0200

    Add main system

--- FSD-Bot.py
+++ FSD-Bot.py
@@ -1,11 +1,73 @@
 from telegram.ext import Updater, CommandHandler
 import os
 from dotenv import load_dotenv
-from dictionary import messageDictionary
+from dictionary import messageDictionary, brainEmojiDictionary, codeEmojiDictionary, alienEmojiDictionary
+from random import *
+import json
+from datetime import datetime
 
 load_dotenv()
 
 TOKEN = os.getenv('TOKEN')
+
+global geek
+geek = 0
+
+def sendEmoji(user, text, update, context):
+
+    path = "data.json"
+    newUser = 1
+    number = randint(1,5)
+
+    if text == "brain" or text == "smart":
+        text1 = "brain"
+        text2 = "smart"
+        EmojiDictionary = brainEmojiDictionary
+    
+    if text == "code" or text == "geek":
+        text1 = "code"
+        text2 = "geek"
+        EmojiDictionary = codeEmojiDictionary
+
+    if text == "geek" or text == "alien":
+        text1 = "geek"
+        text2 = "alien"
+        EmojiDictionary = alienEmojiDictionary
+
+
+    username = user['username']
+    userID = user['id']
+
+    with open(path, 'r') as jsonFile:
+        data = json.load(jsonFile)
+
+    for i in range(len(data)):
+        if data[i]["id"] == userID:
+            if data[i]["text"] == text1 or data[i]["text"] == text2:
+                update.message.reply_text("Une seule commande par utilisateur est autorisée par jour")
+                newUser = 0
+            
+        
+    
+    if newUser == 1:
+        print(f"new User : {username} ")
+        dictionary = {
+            "username": username,
+            "id": userID,
+            "text": text,
+            "number": number,
+            "time": datetime.now().strftime("%H:%M:%S"),
+            "date": datetime.now().strftime("%m/%d/%y")
+        }
+
+        data.append(dictionary)
+
+        with open(path, 'w') as jsonFile:
+            json.dump(data, jsonFile, indent=3)
+
+        update.message.reply_text(EmojiDictionary[number])
+        
+
 
 def startFunction(update, context):
     update.message.reply_text(messageDictionary["start"])
@@ -14,19 +76,36 @@ def helpFunction(update, context):
     update.message.reply_text(messageDictionary["help"])
 
 def brainFunction(update, context):
-    update.message.reply_text("brain")
+    user = update.message.from_user
+    sendEmoji(user, "brain", update, context)
 
 def smartFunction(update, context):
-    update.message.reply_text("smart")
+    user = update.message.from_user
+    sendEmoji(user, "smart", update, context)
 
 def codeFunction(update, context):
-    update.message.reply_text("code")
+    user = update.message.from_user
+    sendEmoji(user, "code", update, context)
 
 def geekFunction(update, context):
-    update.message.reply_text("geek")
+    user = update.message.from_user
+
+    global geek 
+
+    if geek != 1:
+        if randint(1,2) == 1:
+            sendEmoji(user, "code", update, context)
+        else:
+            sendEmoji(user, "alien", update, context)
+        geek = 1
+    else:
+        update.message.reply_text("Une seule commande par utilisateur est autorisée par jour")
+
+
 
 def alienFunction(update, context):
-    update.message.reply_text("alien")
+    user = update.message.from_user
+    sendEmoji(user, "alien", update, context)
 
 def drinkFunction(update, context):
     update.message.reply_text("drink")
@@ -46,6 +125,7 @@ def main():
     updater = Updater(TOKEN, use_context=True)
 
     dp = updater.dispatcher
+
 
     # Base Command
     dp.add_handler(CommandHandler("start", startFunction))
--- dictionary.py
+++ dictionary.py
@@ -24,5 +24,6 @@ alienEmojiDictionary = {
 
 messageDictionary = {
     "start": "FSD-Bot is a chat bot that allows...",
-    "help" : "List of commands /brain or /smart : The bot responds with a random number of brain emojis 🧠.\n/code or /geek : The bot responds with a random number of computer emojis 💻.\n/alien or /geek : The bot responds with a random number of alien emojis 👾. \n/drink or /coffee or /beer or /drink something : The bot responds with messages to simulate the consumption of drinks.\n/start : This command briefly explains the bot's functionalities.\n /help : This command provides a summary explanation of the bot's functionalities."
+    "help" : "List of commands /brain or /smart: The bot responds with a random number of brain emojis 🧠.\n/code or /geek: The bot responds with a random number of computer emojis 💻.\n/alien or /geek : The bot responds with a random number of alien emojis 👾. \n/drink or /coffee or /beer or /drink something : The bot responds with messages to simulate the consumption of drinks.\n/start : This command briefly explains the bot's functionalities.\n /help : This command provides a summary explanation of the bot's functionalities."
 }
+
