a8583cb450e1626c66438496bbd144d882e41dbe
Dwesh163 <ehouarn.duriaux@protonmail.com>
2023-10-03 14:03:43 +0200

    [Fix] #1 add link to the repository

--- dictionary.py
+++ dictionary.py
@@ -23,7 +23,7 @@ alienEmojiDictionary = {
 }
 
 messageDictionary = {
-    "start": "FSD-Bot is a chat bot that allows...",
+    "start": "Pancake bot is a chat bot that allows...\n link to the repository : https://github.com/dwesh163/FSD-Bot/",
     "help" : "List of commands /brain or /smart: The bot responds with a random number of brain emojis 🧠.\n/code or /geek: The bot responds with a random number of computer emojis 💻.\n/alien or /ufo : The bot responds with a random number of alien emojis 👾. \n/drink or /coffee or /beer or /drink something : The bot responds with messages to simulate the consumption of drinks.\n/start : This command briefly explains the bot's functionalities.\n /help : This command provides a summary explanation of the bot's functionalities."
 }
 
